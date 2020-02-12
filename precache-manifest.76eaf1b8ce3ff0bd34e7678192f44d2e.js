6ed504056cdf74157304329fa717884284306d7f
Davis Huang <dhuang@dhuang.local>
2020-02-11 20:35:42 -0800

    Updates

---
+++ precache-manifest.76eaf1b8ce3ff0bd34e7678192f44d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f7d0f6110960d2e2783df22d0d241d",
+    "url": "/odm/index.html"
+  },
+  {
+    "revision": "9bcd933c8065e56412e9",
+    "url": "/odm/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "18ec68d09a13263e3f38",
+    "url": "/odm/static/js/2.b0054bc8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/odm/static/js/2.b0054bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcd933c8065e56412e9",
+    "url": "/odm/static/js/main.b0a89796.chunk.js"
+  },
+  {
+    "revision": "b21f94cfcd8460a4b5e1",
+    "url": "/odm/static/js/runtime-main.f7a15a60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/odm/static/media/logo.5d5d9eef.svg"
+  }
+]);
